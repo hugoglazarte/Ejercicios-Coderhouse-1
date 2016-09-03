864672e3a0476acb2d41a9809a67ef9a8c3a0c32
Hugo Lazarte <hugolazarte@MacBook-Pro-de-Hugo.local>
2016-09-02 23:58:32 -0300

    Subiendo TP

---
+++ tp01/index.js
@@ -0,0 +1,27 @@
+function sumar(){
+	var suma1 = prompt('ingresa un nuevo numero');
+	var suma2 = prompt('ingresa otro mas');
+	var resultadofinal = parseInt(suma1) + parseInt(suma2);
+	alert('el resultado de la Suma es :' + resultadofinal);
+}
+
+function restar(){
+	var resta1 = prompt('ingresa un nuevo numero');
+	var resta2 = prompt('ingresa otro mas');
+	var resultadofinal = parseInt(resta1) - parseInt(resta2);
+	alert('el resultado de la resta es :' + resultadofinal);
+}
+
+function multi(){
+	var multi1 = prompt('ingresa un nuevo numero');
+	var multi2 = prompt('ingresa otro mas');
+	var resultadofinal = parseInt(multi1) * parseInt(multi2);
+	alert('el resultado de la multiplicacion es :' + resultadofinal);
+}
+
+function dividir(){
+	var dividi1 = prompt('ingresa un nuevo numero');
+	var dividi2 = prompt('ingresa otro mas');
+	var resultadofinal = parseInt(dividi1) / parseInt(dividi2);
+	alert('el resultado de la division es :' + resultadofinal);
+}
